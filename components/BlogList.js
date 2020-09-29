aae003bf2094085074b7aeb8c09cb08d167ceeb5
Garrit Franke <garritfranke@gmail.com>
2020-09-29 13:35:34 +0200

    fix: chromium sorting of posts

--- components/BlogList.js
+++ components/BlogList.js
@@ -16,7 +16,9 @@ const BlogList = ({ posts }) => {
       <ul className="list">
         {posts.length > 1 &&
           posts
-            .sort((a, b) => a.frontmatter.date < b.frontmatter.date)
+            // Ternary operator is used to fix chromium sorting
+            // See: https://stackoverflow.com/a/36507611
+            .sort((a, b) => (a.frontmatter.date < b.frontmatter.date ? 1 : -1))
             .map((post) => (
               <Link key={post.slug} href={{ pathname: `/posts/${post.slug}` }}>
                 <a>
