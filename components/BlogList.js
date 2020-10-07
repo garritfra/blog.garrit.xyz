031f17ab6c63bedd3a74932dbd081ba39d637c94
Garrit Franke <garritfranke@gmail.com>
2020-10-07 22:46:21 +0200

    filter drafts from blog list

--- components/BlogList.js
+++ components/BlogList.js
@@ -16,6 +16,7 @@ const BlogList = ({ posts }) => {
       <ul className="list">
         {posts.length > 1 &&
           posts
+            .filter((post) => !post.frontmatter.draft)
             // Ternary operator is used to fix chromium sorting
             // See: https://stackoverflow.com/a/36507611
             .sort((a, b) => (a.frontmatter.date < b.frontmatter.date ? 1 : -1))
