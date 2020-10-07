5c2de336505807481e66606af557511ae6cc762e
Garrit Franke <garritfranke@gmail.com>
2020-10-07 15:58:05 +0200

    fix: nextjs asset hosting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
+  assetPrefix: isProd ? 'https://slashdev.space' : '',
   webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
